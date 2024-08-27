be364c95e516fc6205f9381a9c2fb37a237c0f44
Stephen Wemyss <wemyssstephen@gmail>
2024-08-27 15:31:33 +0100

    Updated HTML to look better

--- email_send.py
+++ email_send.py
@@ -20,59 +20,24 @@ def format_email(db_password=os.environ.get("WEATHER_DB_PASSWORD")):
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width">
             <title>Weather Forecast</title>
-            <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet">
-            <style>
-                body {{
-                    font-family: 'JetBrains Mono', monospace;
-                    line-height: 1.6;
-                    color: #ffffff;
-                    background-color: #121212;
-                    max-width: 600px;
-                    margin: 0 auto;
-                    padding: 20px;
-                }}
-                h1 {{
-                    color: #ffffff}}
-            .forecast-box {{
-                background-color: #1e1e1e;
-                border-radius: 10px;
-                padding: 20px;
-                margin-bottom: 20px;
-            }}
-            .today-heading {{
-                color: #4caf50;
-            }}
-            .tomorrow-heading {{
-                color: #2196f3;
-            }}
-            .weather-icon {{
-                display: block;
-                margin: 10px 0
-                width: 100px
-                height: auto;
-            }}
-            </style>
         </head>
-        <body>
-            <h1>Weather Forecast for {today_forecast["city"]}</h1>
-    
-            <div class="forecast-box">
-                <h2 class="today-heading">Today ({today_forecast["forecast_date"]})</h2>
-                <img src="{today_description["image"]}" alt="Weather Icon" class="weather-icon">
-                <p><strong>Temperature:</strong>{today_forecast["temp_min"]}&deg;C to {today_forecast["temp_max"]}&deg;C</p>
+        <body style="font-family: Ubuntu, 'Roboto Medium', Calibri; font-size: 18px; line-height: 1.6; color: #ffffff; background-color: #121212; max-width: 600px; margin: 0 auto; padding: 20px;">
+            <h1 style="color: #f5cc30; text-align: center;">Weather Forecast for {today_forecast["city"]}</h1>
+        
+            <div style="background-color: #1e1e1e; border-radius: 10px; padding: 20px; margin-bottom: 20px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+                <h2 style="color: #4ec953;">Today ({today_forecast["forecast_date"]})</h2>
+                <img src="{today_description["image"]}" alt="Weather Icon" style="display: block; margin: 10px 0; width: 100px; height: auto;">
+                <p><strong>Temperature:</strong> {today_forecast["temp_min"]}&deg;C to {today_forecast["temp_max"]}&deg;C</p>
                 <p><strong>Weather:</strong> {today_description["description"]}</p>
-    
-                <p><strong>Precipitation:</strong>{today_forecast["precipitation_sum"]}mm - ({today_forecast["precipitation_probability"]}% chance)</p>
+                <p><strong>Precipitation:</strong> {today_forecast["precipitation_sum"]}mm - ({today_forecast["precipitation_probability"]}% chance)</p>
             </div>
-    
-            <div class="forecast-box">
-                <h2 style="color: #1e88e5;">Tomorrow ({tomorrow_forecast["forecast_date"]})</h2>
-                <img src="{tomorrow_description["image"]}" alt="Weather Icon" class="weather-icon">
-                <p><strong>Temperature:</strong> {tomorrow_forecast["temp_min"]}&deg;C to
-                {tomorrow_forecast["temp_max"]}&deg;C</p>
+        
+            <div style="background-color: #1e1e1e; border-radius: 10px; padding: 20px; margin-bottom: 20px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+                <h2 style="color: #21b4f3;">Tomorrow ({tomorrow_forecast["forecast_date"]})</h2>
+                <img src="{tomorrow_description["image"]}" alt="Weather Icon" style="display: block; margin: 10px 0; width: 100px; height: auto;">
+                <p><strong>Temperature:</strong> {tomorrow_forecast["temp_min"]}&deg;C to {tomorrow_forecast["temp_max"]}&deg;C</p>
                 <p><strong>Weather:</strong> {tomorrow_description["description"]}</p>
-    
-                <p><strong>Precipitation:</strong>{tomorrow_forecast["precipitation_sum"]}mm - ({tomorrow_forecast["precipitation_probability"]}% chance)</p>
+                <p><strong>Precipitation:</strong> {tomorrow_forecast["precipitation_sum"]}mm - ({tomorrow_forecast["precipitation_probability"]}% chance)</p>
             </div>
         </body>
         </html>
