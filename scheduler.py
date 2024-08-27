319a96414d46fb23d0902fef078e0f705a682e2b
Stephen Wemyss <wemyssstephen@gmail>
2024-08-27 15:44:50 +0100

    Can be amended to set update schedule.

---
+++ scheduler.py
@@ -0,0 +1,12 @@
+import main
+
+def run_job():
+    """Schedules app to run at 00:30 if computer is on."""
+    main.log("Running weather app...")
+    try:
+        main.run_weather_app()
+    except Exception as e:
+        main.log(f"Error occurred: {str({e})}")
+
+if __name__ == '__main__':
+    run_job()
